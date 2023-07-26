34bf85794a59b6346efa85e85a0f86313b4c8f0a
Danadasa Chan <danadasa108@gmail.com>
2023-07-26 14:20:23 -0700

    remove comment

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,5 @@
 # https://youtu.be/Bk9hlNZc6sE
 # Coding Spot - Coding an Unbeatable Tic Tac Toe AI Using Python and the Minimax Algorithm
-# adding this new comment to practice git checkin / checkout
 
 from constants import *
 from board import Board
